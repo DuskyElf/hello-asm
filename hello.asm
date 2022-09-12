a14724e99f4ed21bfa459e434791f8d408f15149
DuskyElf <clayedison75@gmail.com>
2022-09-12 10:11:34 +0530

    improvements

--- hello.asm
+++ hello.asm
@@ -1,15 +1,17 @@
-global _start
+section .text
+    global _start
 
 _start:
     mov rax, 1          ; write
     mov rdi, 1          ; STDOUT
     mov rsi, message    ; char* buffer
-    mov rdx, 23         ; size_t count
+    mov rdx, size       ; size_t count
     syscall
 
     mov rax, 60         ; exit
     mov rdi, 0          ; exit code
     syscall
 
-section .rodata
+section .data
     message: db "Hello World, from asm!", 10
+    size: equ $ - message
